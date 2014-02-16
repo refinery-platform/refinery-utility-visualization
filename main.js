a44cd9a5e831d19bb9217edb9a816d810af275dc
Anton Xue <antonxue1@gmail.com>
2014-02-16 15:03:01 -0500

    Added the other chart types and some hacky stuff to pie chart, which requires different svg dimensions

--- main.js
+++ main.js
@@ -186,11 +186,143 @@ function draw(chartType) {
     }
 
     function group(data) {
+        var x0 = d3.scale.ordinal()
+            .rangeRoundBands([0, width], 0.1);
 
+        var x1 = d3.scale.ordinal();
+
+        var y = d3.scale.linear()
+            .range([height, 0]);
+
+        var xAxis = d3.svg.axis()
+                .scale(x0)
+                .orient("bottom");
+
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient("left")
+            .tickFormat(d3.format(".2s"));
+
+        var dsets = d3.keys(data[0]).filter(function(key) { return key !== "set"; });
+
+        data.forEach(function(d) {
+            d.datasets = dsets.map(function(name) { return {name: name, value: +d[name]}; });
+        });
+
+        x0.domain(data.map(function(d) { return d.set; }));
+        x1.domain(dsets).rangeRoundBands([0, x0.rangeBand()]);
+        y.domain([0, d3.max(data, function(d) { return d3.max(d.datasets, function(d) { return d.value; })})]);
+
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0, " + height + ")")
+            .call(xAxis);
+
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+            .append("text")
+                .attr("transform", "rotate(-90)")
+                    .attr("y", 6)
+                    .attr("dy", "0.71em")
+                    .style("text-anchor", "end")
+                    .text("Amount");
+
+        var sets = svg.selectAll(".set")
+            .data(data)
+            .enter().append("g")
+            .attr("class", "g")
+            .attr("transform", function(d) { return "translate(" + x0(d.set) + ", 0)"; });
+
+        sets.selectAll("rect")
+            .data(function(d) { return d.datasets; })
+            .enter().append("rect")
+                .attr("width", x1.rangeBand())
+                .attr("x", function(d) { return x1(d.name); })
+                .attr("y", function(d) { return y(d.value); })
+                .attr("height", function(d) { return height - y(d.value); })
+                .style("fill", function(d) { return color(d.name); });
+
+        var legend = svg.selectAll("legend")
+            .data(dsets.slice().reverse())
+            .enter().append("g")
+                .attr("class", "legend")
+                .attr("transform", function(d, i) { return "translate(0, " + i * 20 + ")"; });
+
+        legend.append("rect")
+            .attr("x", width - 18)
+            .attr("width", 18)
+            .attr("height", 18)
+            .style("fill", color);
+
+        legend.append("text")
+            .attr("x", width - 24)
+            .attr("y", 9)
+            .attr("dy", "0.35em")
+            .style("text-anchor", "end")
+            .text(function(d) { return d; });
     }
 
     function pie(data) {
+        radius = Math.min(width, height) / 2;
 
+        var arc = d3.svg.arc()
+            .outerRadius(radius - 10)
+            .innerRadius(0)
+
+        var pie = d3.layout.pie()
+            .sort(null)
+            .value(function(d) { return d.total; });
+
+        // some hacky work around for the pie chart because it requires different svg locations
+        d3.select("svg").remove();
+
+        var svg = d3.select("body").append("svg")
+            .attr("width", width)
+            .attr("height", height)
+            .append("g")
+                .attr("transform", "translate(" + width / 2 + ", " + height / 2 + ")");
+
+        data.forEach(function(d) {
+            d.total = (+d.d1) + (+d.d2) + (+d.d3) + (+d.d4) + (+d.d5);
+        });
+
+        var g = svg.selectAll(".arc")
+            .data(pie(data))
+            .enter().append("g")
+            .attr("class", "arc");
+
+        g.append("path")
+            .attr("d", arc)
+            .style("fill", function(d) { return color(d.data.total); });
+
+        g.append("text")
+            .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+            .attr("dy", "0.35em")
+            .style("text-anchor", "middle")
+            .text(function(d) { return d.data.total; });
+
+        var legend = svg.selectAll(".legend")
+        .data(color.domain().slice().reverse())
+        .enter().append("g")
+            .attr("class", "legend")
+            .attr("transform", function(d, i) { return "translate(0, " + i * 20 + ")"; });
+
+        // add a legend
+        legend.append("rect")
+            .attr("x", radius + 24)
+            .attr("width", 18)
+            .attr("height", 18)
+            .style("fill", color);
+
+        // add text to the legend
+        
+        legend.append("text")
+            .attr("x", radius + 54)
+            .attr("y", 9)
+            .attr("dy", "0.35em")
+            .style("text-anchor", "end")
+            .text(function(d, i) { return data[i].set; });    
     }
 
     // generic legend function to be implemented later
