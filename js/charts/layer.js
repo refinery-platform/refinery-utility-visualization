a67f6ed389f8fc6e5473816e7ac0cac668b70c5d
Anton Xue <antonxue1@gmail.com>
2014-03-06 10:31:10 -0500

    Did some x-axis stuff

--- js/charts/layer.js
+++ js/charts/layer.js
@@ -15,17 +15,11 @@ function layer(data, config) {
         .append("g")
             .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
 
-    console.log("data:");
-    console.log(data);
-
     var mainAxisLabels = d3.keys(data[0]).filter(function(key) { return key !== "set"; });
 
     data.forEach(function(d) {
         d.dataset = mainAxisLabels.map(function(name) { return {name: name, value: +d[name]}; });
     })
-
-    console.log("data after dataset mod: ");
-    console.log(data);
 
     // set up the necessary data structures
     var categoryData = new Array();
@@ -40,16 +34,13 @@ function layer(data, config) {
             }
     }
 
-    console.log("Cateogyr data");
-    console.log(categoryData);
-
     color.domain(mainAxisLabels);
 
     var yScale = d3.scale.linear()
         .domain([0, d3.max(data, function(d) { return d3.max(d.dataset, function(d) { return d.value; })})])
         .range([0, height]);
 
-    var subChartWidth = (width + margin.left + margin.right) / categoryData.length;
+    var subChartWidth = (width + margin.left + margin.right) / categoryData.length - 10;
     var subSvg = [];
     for (var i = 0; i < categoryData.length; i++) {
         subSvg[i] = svg.append("g")
@@ -59,41 +50,45 @@ function layer(data, config) {
                         .append("g");
     }
 
+    // make the y-axis
+    var yAxis = d3.svg.axis()
+                    .scale(d3.scale.linear().domain(yScale.domain().reverse()).range(yScale.range()))
+                    .orient("left");
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
+
+    // make the big x axis
+
     // start plotting the stuff in their unique svg containers
-    //for (var ii = 0; ii < categoryData.length; ii++) {
     for (var ii = 0; ii < categoryData.length; ii++) {
-        console.log("appending rects");
-        subSvg[ii].selectAll("rect")
-            .data(categoryData[ii].setData)
-            .enter().append("rect")
-                .attr("x", function(d, i) { return i * ((subChartWidth - padding) / categoryData.length) + padding + (ii * subChartWidth); })  
-                .attr("y", function(d) { return height - yScale(+d.value); })
-                .attr("width", (subChartWidth - padding) / categoryData.length - barPadding)
-                .attr("height", function(d) { console.log(d.value); return yScale(+d.value); })
-                .attr("fill", color(categoryData[ii].name)); // subject to change
-    }
-
-/*
-    var subChartWidth = width / categoryData.length - 10;
-    for (var ii = 0; ii < categoryData.length; ii++) {
-        console.log("doing some magic");
-
-        svg.selectAll("rect")
+        var g = subSvg[ii].selectAll("rect")
             .data(categoryData[ii].setData)
             .enter().append("rect")
                 .attr("x", function(d, i) { return i * ((subChartWidth - padding) / categoryData.length) + padding + (ii * subChartWidth); })  
                 .attr("y", function(d) { return height - yScale(+d.value); })
                 .attr("width", (subChartWidth - padding) / categoryData.length - barPadding)
                 .attr("height", function(d) { return yScale(+d.value); })
-                .attr("fill", "steelblue"); // subject to change
+                .attr("fill", color(categoryData[ii].name)); // subject to change
 
-*/
+        // TODO: set up some x-axis stuff
+        var xScale = d3.scale.ordinal()
+                        .rangeRoundBands([0, subChartWidth], 0.1)
+                        .domain(data.map(function(d) { return d.set; }));
 
-/*
-        for (var i = 0; i < categoryData[ii].setData.length; i++) {
-            console.log("x: " + (i * ((subChartWidth - padding) / categoryData.length) + padding + (ii * subChartWidth)));
-            console.log("y: " + (height - yScale(+categoryData[ii].setData[i].value)));
-        }
-*/
+        var xAxis = d3.svg.axis()
+                        .scale(xScale)
+                        .orient("bottom");
+
+        subSvg[ii].append("g")
+            .attr("class", "layerXAxis")
+            .attr("transform", "translate(" + (ii * subChartWidth) + ", " + (height) + ")")
+            .call(xAxis);
+
+        subSvg[ii].style("path.fill", "red");
+
+        console.log("x axis translation: " + (ii * subChartWidth))
+    }
     
 }
