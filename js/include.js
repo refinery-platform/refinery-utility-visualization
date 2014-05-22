486711e6700ea1cb5ffc25a165b153575b16f9b4
Anton <antonxue1@gmail.com>
2014-05-21 22:48:25 -0400

    Added working grunt stuff

--- js/include.js
+++ js/include.js
@@ -36,30 +36,30 @@ var tooltip = d3.select("body")
 
 // give events some fancy functions
 var events = {
-        onMouseMove: function(data, g, events) {
-                if (events.tooltipFlag) {
-                        events.tooltip
-                                .html(data.id + "<br>" + data.value)
-                                .style("opacity", 0.9)
-                                .style("top", (d3.event.pageY - 10) + "px")
-                                .style("left", (d3.event.pageX + 10) + "px");
-                }
-        },
-        onMouseOver: function(data, g, events) {
-                events.tooltipFlag = true;
-                d3.select(g.parentNode).selectAll(".bar")
-                        .attr("opacity", 0.6);
-                d3.select(g).attr("opacity", 1);
-        },
-        onMouseOut: function(data, g, events) {
-                events.tooltipFlag = false;
-                d3.select(g.parentNode).selectAll(".bar")
-                        .attr("opacity", 1);
-                events.tooltip.style("opacity", 0);
-        },
-        onClick: function(data, g, events) {
-                console.log("clicky action going on");
-        },
-        tooltip: tooltip,
-        tooltipFlag: false
+    onMouseMove: function(data, g, events) {
+        if (events.tooltipFlag) {
+            events.tooltip
+                .html(data.id + "<br>" + data.value)
+                .style("opacity", 0.9)
+                .style("top", (d3.event.pageY - 10) + "px")
+                .style("left", (d3.event.pageX + 10) + "px");
+        }
+    },
+    onMouseOver: function(data, g, events) {
+        events.tooltipFlag = true;
+        d3.select(g.parentNode).selectAll(".bar")
+                .attr("opacity", 0.6);
+        d3.select(g).attr("opacity", 1);
+    },
+    onMouseOut: function(data, g, events) {
+        events.tooltipFlag = false;
+        d3.select(g.parentNode).selectAll(".bar")
+                .attr("opacity", 1);
+        events.tooltip.style("opacity", 0);
+    },
+    onClick: function(data, g, events) {
+        console.log("clicky action going on");
+    },
+    tooltip: tooltip,
+    tooltipFlag: false
 }
--- js/main.js
+++ js/main.js
@@ -1,27 +1,32 @@
-function draw(chartType, config, data) {
+function draw(chartType, data, config) {
 
-    // delete old svg so graphs aren't cluttered
+    // delete old svg
     d3.select("#" + config.drawTarget).html("");
 
-    // make deep copies for protection against mutation
+    // make deep copies for new ones
     var nData = jQuery.extend(true, {}, data);
     var nConfig = jQuery.extend(true, {}, config);
     var nEvents = jQuery.extend(true, {}, events);
 
-    console.log(nData)
-    console.log(nConfig)
-    console.log(nEvents)
-
-    // general functions depending on graph rendered
     if (chartType === "group") {
         group(nData, nConfig, nEvents);
     } else if (chartType === "layer") {
-    	layer(nData, nConfig, nEvents);
-	} else if (chartType === "simple") {
+        layer(nData, nConfig, nEvents);
+    } else if (chartType === "simple") {
         simplePlain(nData, nConfig, nEvents);
     } else if (chartType === "stack") {
         stack(nData, nConfig, nEvents);
     } else {
         alert("Invalid chart type");
     }
+
+    // try to clean up
+    d3.select("body").selectAll("text")
+        .attr("font-family", "sans-serif")
+        .attr("fill", "black")
+        .attr("font-size", "14px")
+}
+
+var util = {
+  draw: draw
 }
